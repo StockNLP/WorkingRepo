938801c66272edb0cca4530a2eb5867274ef212b
Moses <mosespv407@gmail.com>
2021-12-01 18:18:52 -0800

    adding test directory

---
+++ tests/test_stocknlp.py
@@ -0,0 +1,13 @@
+"""Tests for the stocknlp function"""
+
+import numpy as np
+import unittest
+
+from ** import **
+
+class Teststocknlp(unittest.TestCase):
+
+    def test_smoke(self):
+        """Simple smoke test to make sure function runs"""
+        **
+        return
