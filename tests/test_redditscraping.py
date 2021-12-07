e64de8ab2da03bc530ac86e9c8072ce54304c938
Moses <mosespv407@gmail.com>
2021-12-07 13:41:27 -0800

    test cases

---
+++ tests/test_redditscraping.py
@@ -0,0 +1,37 @@
+"""Tests for the stocknlp function"""
+
+import numpy as np
+import unittest
+
+from WorkingRepo import RedditScraping_copy1
+
+class Teststocknlp(unittest.TestCase):
+
+    def test_smoke(self):
+        """Simple smoke test to make sure function runs"""
+        has_numbers($AMC)
+        return
+    def test_smoke(self):
+        """Simple smoke test to make sure function runs"""
+        get_subreddit_column(robinhood, )
+        return
+    def test_smoke(self):
+        """Simple smoke test to make sure function runs"""
+        cashtags(, )
+        return
+    def test_smoke(self):
+        """Simple smoke test to make sure function runs"""
+        __flatten_list(, )
+        return
+    def test_smoke(self):
+        """Simple smoke test to make sure function runs"""
+        add_to_df(, )
+        return
+    def test_smoke(self):
+        """Simple smoke test to make sure function runs"""
+        has_special_chars(, )
+        return
+    def test_smoke(self):
+        """Simple smoke test to make sure fucntion runs"""
+        
+
